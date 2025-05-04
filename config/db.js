afa7eed57ddb2fdf61c647e3d06ad56daea8291c
wkdtpdlf <sheshe1809@gmail.com>
2025-05-04 14:41:25 +0900

    [🌟 Feat] 테스트

---
+++ config/db.js
@@ -0,0 +1,17 @@
+// config/db.js
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ MongoDB 로컬 연결 완료");
+  } catch (error) {
+    console.error("❌ MongoDB 연결 실패:", error.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
