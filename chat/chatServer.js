db709cda963f35b562553ab622e742334384e6b3
wkdtpdlf <sheshe1809@gmail.com>
2025-05-30 15:39:04 +0900

    Update 백엔드 수정

--- chat/chatServer.js
+++ chat/chatServer.js
@@ -1,12 +1,12 @@
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const setupSocketEvents = require("./chatHandler");
-const app = require("../server"); // 기존 express app
+const app = require("../app");
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: ["http://localhost:3000", "http://192.168.35.111:3000"],
+    origin: ["http://localhost:3000", "http://172.30.1.91:3000"],
     methods: ["GET", "POST"],
     credentials: true,
   },
